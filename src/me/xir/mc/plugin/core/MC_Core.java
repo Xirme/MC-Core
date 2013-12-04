0ac885fa73ce78bdee3fbb67d192b25d1b400a3f
Cyberpew <root@cyberpew.me>
2013-12-04 14:28:15 -0800

    Let's do some configuration generation.

--- src/me/xir/mc/plugin/core/MC_Core.java
+++ src/me/xir/mc/plugin/core/MC_Core.java
@@ -1,5 +1,6 @@
 package me.xir.mc.plugin.core;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,7 +14,22 @@ public class MC_Core extends JavaPlugin {
 	}
 
 	public void onEnable() {
+		
 		log.info("[MC-Core] is online.");
+		loadConfiguration();
+	}
+	
+	public void loadConfiguration() {
+		int serverid = this.getConfig().getInt("general.server-id");
+		File f = new File(this.getDataFolder(), "config.yml");
+		if (!f.exists()) {
+			log.info("[MC-Core] Generating default configuration.");
+			saveDefaultConfig();
+			log.info("[MC-Core] URGENT: config.yml has default values!");
+		} else {
+			log.info("[MC-Core] Configuration file found.");
+			log.info("This is server ID " + serverid + ".");
+		}
 	}
 	
 }
